57615fae866ac157116ac9a4ed0752afd09bd47d
ElisaSer <elisabetta.serfilippi@gmail.com>
2024-06-10 18:26:30 +0200

    Create Cioccolatino.java

---
+++ Cioccolatino.java
@@ -0,0 +1,13 @@
+public class Cioccolatino {
+
+    public String forma;
+    public String ripieno;
+
+    public Cioccolatino(String forma, String ripieno) {
+        this.forma = forma;
+        this.ripieno = ripieno;
+    }
+
+    
+
+}
