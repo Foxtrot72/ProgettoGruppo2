5a25523898678d48b6ee1f2234fd60e3e72f58d3
ElisaSer <elisabetta.serfilippi@gmail.com>
2024-06-10 21:24:22 +0200

    Update Cioccolatino.java

--- Cioccolatino.java
+++ Cioccolatino.java
@@ -3,9 +3,11 @@ public class Cioccolatino extends Cioccolato{
     public String forma;
     public String ripieno;
 
-    public Cioccolatino(String forma, String ripieno) {
-        super(forma, ripieno);
+    public Cioccolatino(String tipoCioccolato, float percentuale, int numeroCioccolatini, String forma, String ripieno) {
+        super(tipoCioccolato, percentuale, numeroCioccolatini);
     }
+
+  
 
     @Override
     public void produce(){
